41a5f690c1b66400262472320ccb5f487daf6f34
RoseofKendy <162413453+RoseofKendy@users.noreply.github.com>
2025-06-25 22:47:31 +0300

    Update recipe_routes.py

--- server/routes/recipe_routes.py
+++ server/routes/recipe_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.services.recipe_service import RecipeService
 from app.utils.decorators import validate_json
+from app.models import Recipe, Favorite
 
 recipes_bp = Blueprint('recipes', __name__, url_prefix='/api/recipes')
 
