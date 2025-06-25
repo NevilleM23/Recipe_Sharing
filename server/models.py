1a6c4e5e7b1c733ecc8383fbd7aebbf5069746a4
Sharon Kendi <sharonkendi35@gmail.com>
2025-06-25 08:49:31 +0300

    Updates class Recipe

--- server/models.py
+++ server/models.py
@@ -11,3 +11,14 @@ class User(db.Model):
     password_hash = db.Column(db.String(255))
     recipes = db.relationship("Recipe", backref="user", lazy=True)
     favorites = db.relationship("Favorite", backref="user", lazy=True)
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    steps = db.Column(db.Text)
+    cook_time = db.Column(db.Integer)
+    difficulty = db.Column(db.String(50))
+    budget_rating = db.Column(db.String(50))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    ingredients = db.relationship("RecipeIngredient", backref="recipe", lazy=True)
+    favorites = db.relationship("Favorite", backref="recipe", lazy=True)
