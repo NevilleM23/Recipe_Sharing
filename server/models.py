4d73f05f7b10c93deda838eee70b4f086c7f90ae
Sharon Kendi <sharonkendi35@gmail.com>
2025-06-25 08:57:29 +0300

    Updates class Favorite

--- server/models.py
+++ server/models.py
@@ -50,3 +50,9 @@ class PriceEntry(db.Model):
     ingredient_id = db.Column(db.Integer, db.ForeignKey("ingredient.id"), nullable=False)
     market_id = db.Column(db.Integer, db.ForeignKey("market.id"), nullable=False)
     price = db.Column(db.Float)
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipe.id"), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
