e05a787af774dcff17341fdb199e852082b61d45
Neville Muli <neville.muli@gmail.com>
2025-06-30 14:56:35 +0300

    switches db to postgresql, ready for deployment

--- app/config.py
+++ app/config.py
@@ -6,7 +6,10 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance/recipe_app.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL',
+        'postgresql+psycopg2://nevillem:mypassword@localhost/recipe_sharing'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt-dev-secret-key')
     JWT_ACCESS_TOKEN_EXPIRES = 3600  # 1 hour in seconds
